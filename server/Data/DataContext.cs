678e7821a109cd894ce4731189a4021201e00316
ari <ari65040@gmail.com>
2023-06-23 01:27:17 +0300

    design basik diagram

--- server/Data/DataContext.cs
+++ server/Data/DataContext.cs
@@ -8,7 +8,6 @@ namespace server.Data
         public DataContext(DbContextOptions options) : base(options) { }
 
         public DbSet<ChannelsEO> channels { get; set; }
-
         public DbSet<Sub_channelsEO> sub_channels { get; set; }
         public DbSet<VideosEO> videos { get; set; }
         public DbSet<Member_profileEO> member_profile { get; set; }
